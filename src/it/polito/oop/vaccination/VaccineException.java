7c75d9aba69069923765ca241841820f96749c8a
Antonio Vetrò <antonio.vetro@polito.it>
2021-11-02 17:29:40 +0100

    test nuovi

--- src/it/polito/oop/vaccination/VaccineException.java
+++ src/it/polito/oop/vaccination/VaccineException.java
@@ -2,6 +2,6 @@ package it.polito.oop.vaccination;
 
 public class VaccineException extends Exception {
 	private static final long serialVersionUID = 1L;
-	public VaccineException() {super("STOP - Vaccine system error");}
+	public VaccineException() {super("STOP!! - Vaccine system error");}
 	public VaccineException(String msg ) {super(msg);}
 }
